37d3308ad90b7f2a244255c03d741eb3889773e6
GrantSwajian <ggswajian5@gmail.com>
2021-04-01 12:51:24 -0800

    Add files via upload

---
+++ neonest.py
@@ -0,0 +1,13 @@
+from commonsensors import GetData
+from commonsensors import Average
+GetData(12, 6)
+print(mq135)
+print(mq2)
+
+
+# s.x.0 = mq135
+# s.x.1 = mq2
+# s.x.2 = mq3
+# s.x.3 = mq4
+# s.x.4 = mq5
+# s.x.5 = mq7
