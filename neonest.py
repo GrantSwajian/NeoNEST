42e32bc07953bd7743ff896c6489cc941c416445
GrantSwajian <ggswajian5@gmail.com>
2021-04-01 18:09:17 -0800

    Add files via upload

--- neonest.py
+++ neonest.py
@@ -1,13 +1,30 @@
+import sys
 from commonsensors import GetData
 from commonsensors import Average
-GetData(12, 6)
-print(mq135)
-print(mq2)
+from time import sleep
+import matplotlib.pyplot as plt
+import numpy as np
 
+print("Hello World")
+a,b,c,d,e,f = GetData(25, 6)
+mq135average = Average(a)
+mq2average = Average(b)
+mq3average = Average(c)
+mq4average = Average(d)
+mq5average = Average(e)
+mq7average = Average(f)
+sleep(1)
 
-# s.x.0 = mq135
-# s.x.1 = mq2
-# s.x.2 = mq3
-# s.x.3 = mq4
-# s.x.4 = mq5
-# s.x.5 = mq7
+xpoints = np.array ([0,12])
+ypoints = np.array([0,200])
+plt.plot(a,marker = '*')
+plt.plot(b,marker = '*')
+plt.plot(c,marker = '*')
+plt.plot(d,marker = '*')
+plt.plot(e,marker = '*')
+plt.plot(f,marker = '*')
+plt.show()
+sleep(10)
+ 
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
